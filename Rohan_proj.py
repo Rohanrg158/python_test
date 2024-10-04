b3b8a7730c69ec258d89355b9df98e980242a897
Rohanrg158 <rohan.renji158@gmail.com>
2024-10-04 16:05:22 +0530

    Add files via upload

---
+++ Rohan_proj.py
@@ -0,0 +1,66 @@
+'''
+print("Hello world")
+
+print ("Hello\nWorld")
+
+if 5>2:
+    print("Five is greater than two")
+
+
+
+if 5>2:
+    print("Five is greater than two")
+
+
+x=10
+y="Rohan"
+print (x,y)
+
+x=str(3)
+y=int(3)
+z=float(3)
+print(x,y,z)
+
+x=5
+y="Rohan"
+print(type(x))
+print(type(y))
+
+x, y, z= "orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+x="Python"
+y=" is"
+z=" awesome"
+print(x + y + z)
+
+username=input("Enter username: ")
+print("Username is: ",username)
+
+a="Hello "
+b="World"
+c=a + b
+print(c)
+
+a= "Hello World"
+print(a[4])
+
+a= "Hello World"
+print(a[4:8])
+'''
+
+
+
+
+
+
+
+
+
