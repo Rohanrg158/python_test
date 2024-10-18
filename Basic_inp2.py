09881a3da3c2642a2da84306e31f44a7fe3ef3e6
RRG <rohan.renji158@gmail.com>
2024-10-18 16:17:39 +0530

    Update Basic_inp2.py

--- Basic_inp2.py
+++ Basic_inp2.py
@@ -1,5 +1,7 @@
 '''
-Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+Author: Rohan 
+Date: 18-10-2024
+Description: Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
 '''
 
 str1="Hello world!"
