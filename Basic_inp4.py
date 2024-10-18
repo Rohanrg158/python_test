231284c29198ddf095b1ee48ab0a54198c52a85f
Rohanrg158 <rohan.renji158@gmail.com>
2024-10-18 15:24:15 +0530

    Add files via upload

---
+++ Basic_inp4.py
@@ -0,0 +1,11 @@
+'''
+Write a Python program that uses functions from the math module to perform the following operations on a number provided by the user:
+'''
+
+import math
+number=int(input("Enter a number:"))
+sqrroot=print("Square root of",number,":",math.sqrt(number))
+fact=print("Factorial of",number,":",math.factorial(number))
+power=print(number,"raised to the power 2:",number**2)
+
+
