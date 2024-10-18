93b61c0c19e81e827dc7278866fe5eb18397adec
RRG <rohan.renji158@gmail.com>
2024-10-18 16:18:17 +0530

    Update Basic_inp5.py

--- Basic_inp5.py
+++ Basic_inp5.py
@@ -1,5 +1,7 @@
 '''
-Write a Python program to print the following string in a specific format (see the output).
+Author: Rohan 
+Date: 18-10-2024
+Description: Write a Python program to print the following string in a specific format (see the output).
 Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
 Output :
 
