a4066e4fa6d1edf6152bc32a28525d0353b7d48f
Rohanrg158 <rohan.renji158@gmail.com>
2024-10-18 14:31:29 +0530

    Add files via upload

---
+++ Basic_inp.py
@@ -0,0 +1,15 @@
+'''
+Author: Rohan R
+Date: 18-10-2024
+Description:Write a Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Expected Output:
+Hello, John! You are 25 years old.
+'''
+
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+print("Hello,",name,"! You are", age, "years old.")
+
+
+
+
---
+++ Basic_inp2.py
@@ -0,0 +1,8 @@
+'''
+Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+
+str1="Hello world!"
+new=str1[6:11]
+a=new+" "+"Everyone!"
+print(a)
