c5b75ca6138db8f8eb52c302fe51b97498d2db62
Rohanrg158 <rohan.renji158@gmail.com>
2024-10-18 15:04:35 +0530

    Add files via upload

---
+++ Basic_inp3.py
@@ -0,0 +1,15 @@
+'''
+Write a Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+'''
+a=20
+b=10
+print("sum: ",a+b,"Division: ",a/b)
+var1=(a>b)
+print("Is a greater than b?:",var1)
+var2=(a==b)
+print("Are a and b equal?:",var2)
+print("Logical And:",(a>b)and(b<a))
+print("Logical OR:",(a==b)or(a<b))
+
+
+
