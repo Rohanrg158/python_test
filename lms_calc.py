c568941b3906d3419fcdc4f39a01c410d368f157
RRG <rohan.renji158@gmail.com>
2024-10-19 15:26:51 +0530

    Add files via upload

---
+++ lms_calc.py
@@ -0,0 +1,15 @@
+'''
+Author: Rohan
+Date: 19-10-2024
+Simple desktop calculator using Python. Only the five basic arithmetic operators.
+'''
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+num3=int(input("Enter the third number: "))
+print("The sum of num1 and num2 is: ",num1+num2)
+print("The difference when num2 is subtracted from num1 is: ",num1-num2)
+print("The product of num1 and num2 is: ",num1*num2)
+print("The quotient when num1 is divided by num2 is: ",num1/num2)
+print("The remainder when num1 is divided by num2 is: ",num1%num2)
+print("The result of (num1+num2)*num3/2 is: ",(num1 + num2) * num3 / 2)
