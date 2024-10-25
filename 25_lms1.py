146e2a6eaf0b4333a97b376d98799f9be8fcef08
RRG <rohan.renji158@gmail.com>
2024-10-25 14:33:59 +0530

    Update 25_lms1.py

--- 25_lms1.py
+++ 25_lms1.py
@@ -1,3 +1,10 @@
+'''
+Author: Rohan R
+Date: 25-10-2024
+Description:Write a Python program to find the largest of three numbers. The program should take three numbers as input from the user and determine which one is the largest. 
+Use conditional statements to compare the numbers.
+'''
+
 num1=int(input("Enter the first number: "))
 num2=int(input("Enter the second number: "))
 
