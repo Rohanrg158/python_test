dbf9e3b56af55a77ffa1c36ae08fa53daf664dbd
RRG <rohan.renji158@gmail.com>
2024-10-25 14:32:23 +0530

    Add files via upload

---
+++ 25_lms1.py
@@ -0,0 +1,19 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+if num1>num2:
+    print(num1,"is greater than",num2)
+else:
+    print(num2,"is greater than",num2)
+
+if num1>0:
+    print(num1,"is positive")
+else:
+    print(num1,"is negative")
+
+if num2>0:
+    print(num2,"is positive")
+else:
+    print(num2,"is negative")
+
+
