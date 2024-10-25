d2f1c8687d1181294670af4d9175a5a098cfa7d2
RRG <rohan.renji158@gmail.com>
2024-10-25 14:53:27 +0530

    Add files via upload

---
+++ 25_lms.py
@@ -0,0 +1,18 @@
+'''
+Name: Rohan
+Date: 20-10-2024
+AIM: Write a Python program to find the largest of three numbers.
+The program should take three numbers as input from the user and determine which one is the largest. Use conditional statements to compare the numbers.
+'''
+
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+num3=int(input("Enter the third number: "))
+
+if num1>num2 and num1>num3:
+    print(num1,"is greater")
+elif num2>num3 and num2>num1:
+    print(num2,"is greater")
+else:
+    print(num3,"is greater")
