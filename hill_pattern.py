d8d488f9d76bf61adf1883809b0a62a78d460565
RRG <rohan.renji158@gmail.com>
2024-11-22 15:57:04 +0530

    Add files via upload

---
+++ hill_pattern.py
@@ -0,0 +1,25 @@
+n=int(input("Enter a number: "))
+
+for i in range(1,n+1):
+    for j in range(i):
+        print('*', end=" ")
+    print()
+
+for k in range(n+1,0,-1):
+    for l in range(k):
+        print('*', end=" ")
+    print()
+
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(1, 2 * i):
+        print("*", end="")
+    print()
+
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print(" ", end="")
+    for k in range(2*i-1):
+        print("*", end="")
+    print()
