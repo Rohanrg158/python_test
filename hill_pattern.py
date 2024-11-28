5d744d13dc4819e70a4b6a2a7e850a8898edf971
RRG <rohan.renji158@gmail.com>
2024-11-28 23:10:15 +0530

    hill_pattern.py

--- hill_pattern.py
+++ hill_pattern.py
@@ -1,15 +1,18 @@
 n=int(input("Enter the number of rows: "))
 
+# Increasing triangle
 for i in range(1,n+1):
     for j in range(i):
         print('*', end=" ")
     print()
 
+# Decreasing triangle
 for k in range(n+1,0,-1):
     for l in range(k):
         print('*', end=" ")
     print()
 
+# Hill pattern
 for i in range(1, n + 1):
     for j in range(n - i):
         print(" ", end="")
@@ -17,6 +20,7 @@ for i in range(1, n + 1):
         print("*", end="")
     print()
 
+# Reverse hill pattern
 for i in range(n,0,-1):
     for j in range(n-i):
         print(" ", end="")
