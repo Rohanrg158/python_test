c8e1ee4c3123a86095c537c0909ce4af2037c54f
RRG <rohan.renji158@gmail.com>
2024-12-06 15:48:13 +0530

    fibonacci_number.py

--- fibonacci_number.py
+++ fibonacci_number.py
@@ -1,12 +1,10 @@
-def fibonacci(n):
+def fibonacci(num):
+    list1=[]
     num1=0
     num2=1
-    new_number=num2
-    count=1
-    while count<=n-1:
-        print(new_number,end=" ")
-        count+=1
-        num1, num2= num2, new_number
-        new_number =num1+num2
-    print()
-fibonacci(9)
+    for i in range (num):
+        list1.append(num1)
+        num1,num2=num2,num1+num2
+    return list1
+
+print(fibonacci(9))
